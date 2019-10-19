6d40da4e97d8cffc5fab7e001ff0bfed0e748f8d
Azure <cvbnm0002@gmail.com>
2019-10-20 01:32:36 +0800

    Add OutputWindowDebuger for showing message.

---
+++ PluginDeployTool/PluginDeployTool/OutputWindow/OutputWindowHelper.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+using System;
+
+namespace PluginDeployTool.OutputWindow
+{
+    sealed class OutputWindowHelper
+    {
+        private static readonly OutputWindowHelper m_instance = new OutputWindowHelper();
+
+        public static OutputWindowHelper GetInstance()
+        {
+            return m_instance;
+        }
+
+        public void Write(String message)
+        {
+            ThreadHelper.ThrowIfNotOnUIThread();
+
+            var outputWindow = DeployPluginPackage.GetGlobalService(typeof(SVsOutputWindow)) as IVsOutputWindow;
+
+            Guid guid = VSConstants.GUID_OutWindowDebugPane;
+            outputWindow.GetPane(ref guid, out IVsOutputWindowPane outputPanel);
+            outputPanel.OutputString(message + Environment.NewLine);
+            outputPanel.Activate();
+        }
+    }
+}
